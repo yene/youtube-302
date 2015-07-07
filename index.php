fce88ff39ed9033ec8d540522ab59776ee24a5ce
yene <yene@cocoadojo.com>
2015-07-07 16:10:41 +0200

    added youtube api call

--- index.php
+++ index.php
@@ -2,12 +2,20 @@
 require_once "apiKey.php";
 
 if (isset($_GET["q"]) && !empty($_GET["q"])) {
-  $url = "https://www.youtube.com/results?search_query=";
-  header("Location: " . $url);
-  die();
+  $apiURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&order=viewCount&type=video&key=" . $apiKey . "&q=" . urlencode($_GET["q"]);
+  $apiResult = file_get_contents($apiURL);
+  $json = json_decode($apiResult, true);
+
+  if (count($json["items"]) == 1) {
+    $videoID = $json["items"][0]["id"]["videoId"];
+    $url = "https://www.youtube.com/watch?v=" . $videoID;
+    header("Location: " . $url);
+    die();
+  } else {
+    die("Error: no video found");
+  }
 }
 ?>
-
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
@@ -27,9 +35,10 @@ if (isset($_GET["q"]) && !empty($_GET["q"])) {
         <fieldset>
           <label for="q">Search for a youtube video and get redirected to the first result:</label>
           <input name="q" type="text" id="q" size="25" placeholder="Rick Astley" autofocus/>
-          <button type="submit">Search & redirect</button>
+          <button type="submit">Play First Result</button>
         </fieldset>
       </form>
+      <p><small>You can add <code>?q=rick+astley</code> to the URL for an instant redirect.</small></p>
     </article>
     <footer>
         <nav>
