bd1d39307b7aff15287f1e9b7df3e6e50b293c4a
yene <yene@cocoadojo.com>
2015-07-07 15:34:17 +0200

    styled page and added script to start server

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Youtube search</title>
+  <!-- Normalize.css is a customisable CSS file that makes browsers render all elements more consistently and in line with modern standards. -->
+  <link rel="stylesheet" media="screen" href="https://raw.github.com/necolas/normalize.css/master/normalize.css">
+  <link rel="stylesheet" type="text/css" href="//yegor256.github.io/tacit/tacit.min.css"/>
+  </style>
+</head>
+<body>
+  <section>
+    <article>
+      <h1>Search for a youtube video.</h1>
+      <p>you will get redirected to the first result<p>
+      <form method="get" action="">
+        <fieldset>
+          <label for="q">Search for youtube video:</label>
+          <input name="q" type="text" id="q" size="25" placeholder="Rick Astley"/>
+          <button type="submit">Subscribe</button>
+        </fieldset>
+      </form>
+    </article>
+    <footer>
+        <nav>
+          <ul>
+            <li>
+              <small>Made by <a href="http://yannickweiss.com">Yannick Weiss</a></small>
+            </li>
+          </ul>
+        </nav>
+    </footer>
+  </section>
+</body>
+</html>
